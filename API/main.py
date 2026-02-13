c32c1084de4d8c0a70ede66b8a2d9e19d2415248
SLara24 <santiagolara.pj@hotmail.com>
2026-02-13 15:13:50 +0000

    Update api with accessibility score calculation

--- API/main.py
+++ API/main.py
@@ -4,9 +4,23 @@
 # Importing necessary libraries
 from fastapi import FastAPI
 from API.routers import accessibility, spatial
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Creating the FastAPI app
 app = FastAPI(title="Green Spaces Accessibility API")
+
+origins = ["*"]   # allow everyone for now
+
+# Adding CORS middleware to allow cross-origin requests 
+# REMEMBER TO CHANGE THIS IN PRODUCTION!!!!!!!!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Root endpoint
 @app.get("/")
